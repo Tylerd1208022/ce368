a8d514243d7d56950d9d9210ad5bed76f3a58747
Andy Xu <axl6282@scarecrow.ece.northwestern.edu>
2023-02-20 14:02:40 -0600

    Working hist #1

--- labs/src/lab3/opt_2dhisto.cu
+++ labs/src/lab3/opt_2dhisto.cu
@@ -6,46 +6,49 @@
 #include "util.h"
 #include "ref_2dhisto.h"
 
-__global__ void histogramKernel(uint* d_result, uint* d_data, int dataN, int BIN_COUNT){
+__global__ void histogramKernel(uint* d_result, uint* d_data, int dataN, int BIN_COUNT){ // should use BIN_COUNT somehow
     const int globalTid = blockIdx.x * blockDim.x + threadIdx.x;
     const int numThreads = blockDim.x * gridDim.x;
     const int BC = 1024;
+
     __shared__ uint s_Hist[BC];
-    __shared__ int sum;
-    for(int pos = threadIdx.x; pos<BC; pos+=blockDim.x){
+
+    // initialize hist to 0
+    for(int pos = threadIdx.x; pos < BC; pos += blockDim.x){
         s_Hist[pos] = 0;
     }
+
     __syncthreads();
-    for(int pos = threadIdx.x; pos < INPUT_HEIGHT * INPUT_WIDTH; pos += 32){
-        int x = pos%INPUT_WIDTH;
-        int y = pos/INPUT_WIDTH;
-        uint data = d_data[x + (y*4096)];
-        if (s_Hist[data] < 255) atomicAdd(s_Hist + data,1);
+
+    // compute hist
+    for(int pos = threadIdx.x; pos < INPUT_HEIGHT * INPUT_WIDTH; pos += numThreads){
+        int x = pos % INPUT_WIDTH;
+        int y = pos / INPUT_WIDTH;
+        uint data = d_data[x + (y * BC * 4)];
+        if (s_Hist[data] < 255) atomicAdd(s_Hist + data, 1);
     }
+
     __syncthreads();
-    //atomicAdd(&sum,1);
-    for(int i = 0; i < 1024; i++) s_Hist[i] = i;
-    for(int pos = threadIdx.x * 4; pos + 4 <= 1024; pos += blockDim.x * 4){
+
+    // merge bins
+    for(int pos = threadIdx.x * 4; pos + 4 <= BC; pos += blockDim.x * 4){
         uint merged_bin = 0;
         merged_bin += s_Hist[pos];
         merged_bin += s_Hist[pos + 1] << 8;
         merged_bin += s_Hist[pos + 2] << 16;
         merged_bin += s_Hist[pos + 3] << 24;
-        d_result[pos/4] = merged_bin;
-        __syncthreads();
+        atomicAdd(d_result + pos/4, merged_bin);
     }
-    
 }
 
-void opt_2dhisto(uint* d_result, uint* d_data, int dataN,int BIN_COUNT)
+void opt_2dhisto(uint* d_result, uint* d_data, int dataN, int BIN_COUNT)
 {
     /* This function should only contain a call to the GPU 
        histogramming kernel. Any memory allocations and
        transfers must be done outside this function */
-       dim3 blockSize = (1,1,32);
-       dim3 gridSize = (1,1,1);
-       histogramKernel<<<gridSize,blockSize>>>(d_result, d_data, dataN,BIN_COUNT);
-      //for(int i = 0; i < 256; i++) *(d_result + i) = i;
+       dim3 blockSize = (32, 1, 1);
+       dim3 gridSize = (1, 1, 1);
+       histogramKernel<<<gridSize,blockSize>>>(d_result, d_data, dataN, BIN_COUNT);
 }
 
 /* Include below the implementation of any other functions you need */
